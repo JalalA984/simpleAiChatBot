549f4d2f2bbf84de098affe8bacd586529c81977
Jalal <128756237+JalalA984@users.noreply.github.com>
2023-08-21 19:51:18 -0400

    Update .gitignore and main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tensorflow
+import random
+import json
+
+# TODO: tflearn import causing bug
+#import tflearn
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+print(data)
+
+
